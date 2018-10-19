e6da5b0bcac434076bb7d9238bdc7c7b1ea580b5
Aleksei <alexei@0113@gmail.com>
2018-10-19 16:21:23 +0700

    add login

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,9 +3,11 @@ import { connect } from 'react-redux'
 import { User } from '../components/User'
 import { Page } from '../components/Page'
 import { getPhotos } from '../actions/PageActions'
+import { handleLogin } from '../actions/UserActions'
 class App extends Component {
 	render() {
-		const { user, page, getPhotosAction } = this.props
+		// вытащили handleLoginAction из this.props
+		const { user, page, getPhotosAction, handleLoginAction } = this.props
 		return (
 			<div className="app">
 				<Page
@@ -14,20 +16,28 @@ class App extends Component {
 					isFetching={page.isFetching}
 					getPhotos={getPhotosAction}
 				/>
-				<User name={user.name} />
+				{/* добавили новые props для User */}
+				<User
+					name={user.name}
+					isFetching={user.isFetching}
+					error={user.error}
+					handleLogin={handleLoginAction}
+				/>
 			</div>
 		)
 	}
 }
 const mapStateToProps = store => {
 	return {
-		user: store.user,
+		user: store.user, // вытащили из стора (из редьюсера user все в переменную thid.props.user)
 		page: store.page,
 	}
 }
 const mapDispatchToProps = dispatch => {
 	return {
 		getPhotosAction: year => dispatch(getPhotos(year)),
+		// "приклеили" в this.props.handleLoginAction функцию, которая умеет диспатчить handleLogin
+		handleLoginAction: () => dispatch(handleLogin()),
 	}
 }
 export default connect(
