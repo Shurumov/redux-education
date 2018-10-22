767f2eb32f74aeb96605f1f437db9f058c262e06
Aleksei <alexei@0113@gmail.com>
2018-10-22 11:46:39 +0700

    final

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,32 +3,45 @@ import PropTypes from 'prop-types'
 export class Page extends React.Component {
 	onBtnClick = e => {
 		const year = +e.currentTarget.innerText
-		this.props.getPhotos(year)
+		this.props.getPhotos(year) // setYear -> getPhotos
+	}
+	renderButtons = () => {
+		const years = [2018, 2017, 2016, 2015, 2014]
+		return years.map((item, index) => {
+			return (
+				<button key={index} className="btn" onClick={this.onBtnClick}>
+					{item}
+				</button>
+			)
+		})
+	}
+	renderTemplate = () => {
+		const { photos, isFetching, error } = this.props
+		if (error) {
+			return <p className="error">Во время загрузки фото произошла ошибка</p>
+		}
+		if (isFetching) {
+			return <p>Загрузка...</p>
+		} else {
+			return photos.map(entry => (
+				<div key={entry.id} className="photo">
+					<p>
+						<img src={entry.sizes[0].url} alt="" />
+					</p>
+					<p>{entry.likes.count} ❤</p>
+				</div>
+			))
+		}
 	}
 	render() {
-		const { year, photos, isFetching } = this.props
+		const { year, photos } = this.props
 		return (
 			<div className="ib page">
-				<p>
-					<button className="btn" onClick={this.onBtnClick}>
-						2018
-					</button>{' '}
-					<button className="btn" onClick={this.onBtnClick}>
-						2017
-					</button>{' '}
-					<button className="btn" onClick={this.onBtnClick}>
-						2016
-					</button>{' '}
-					<button className="btn" onClick={this.onBtnClick}>
-						2015
-					</button>{' '}
-					<button className="btn" onClick={this.onBtnClick}>
-						2014
-					</button>
-				</p>
-				<h3>{year} год</h3>
-				{}
-				{isFetching ? <p>Загрузка...</p> : <p>У тебя {photos.length} фото.</p>}
+				<p>{this.renderButtons()}</p>
+				<h3>
+					{year} год [{photos.length}]
+				</h3>
+				{this.renderTemplate()}
 			</div>
 		)
 	}
@@ -37,5 +50,6 @@ Page.propTypes = {
 	year: PropTypes.number.isRequired,
 	photos: PropTypes.array.isRequired,
 	getPhotos: PropTypes.func.isRequired,
+	error: PropTypes.string,
 	isFetching: PropTypes.bool.isRequired,
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/configureStore'
-import App from './containers/App'
+import App from './components/App' // изменили путь
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 ReactDOM.render(
 	<Provider store={store}>
-		<div className="app">
-			<App />
-		</div>
+		<App />
 	</Provider>,
 	document.getElementById('root')
 )
